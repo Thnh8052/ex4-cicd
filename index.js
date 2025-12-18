ca721960bda47a45ccd98e3fa94d956cdd561d27
Thnh8052 <vuthanh250804@gmail.com>
2025-12-18 20:14:11 +0700

    checking CI/CD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-    res.status(200).send("Xin chào bạn");
+    res.status(200).send("CI/CD checking");
 });
 
 // Start server
